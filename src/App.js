206c1bf1c328f48ba82530cf225789a773ec24e0
TheMTorres29 <39773958+TheMTorres29@users.noreply.github.com>
2023-08-02 20:07:39 -0700

    quick

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from "react";
+import Axios from "axios";
 
 function App() {
+  const [pokemonName, setPokemonName] = useState("");
+  const [chosen, setChosen] = useState(false);
+  const [pokemonData, setPokemonData] = useState({
+    name: "", 
+    sprite: "",
+    type: "",
+  });
+
+  const searchPokemon = () => {
+    Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((response)=> {
+      setPokemonData({
+        name: pokemonName, 
+        sprite: response.data.sprites.front_default,
+        type: response.data.types[0].type.name,
+        });
+        setChosen(true);
+      }
+      
+    )
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Title Section */}
+      <div className="title-section">
+        <h1 className="app-title">PokeAPI Practice</h1>
+        <div className="search-container">
+          <input className="poke-input" type="text" onChange={(event) => {setPokemonName(event.target.value)}}/>
+          <button onClick={searchPokemon} className="poke-search">Search</button>
+        </div>
+      </div>
+
+      {/* Pokemon Being Displayed Section */}
+      <div className="pokemon-section">
+        {!chosen ? (<h1>Choose your Pokemon!</h1>) :
+        (
+          <>
+            <h1>{pokemonData.name}</h1>
+            <img src={pokemonData.sprite} />
+            <h3>Type: {pokemonData.type}</h3>
+          </>
+        )}
+      </div>
+      
     </div>
   );
 }
